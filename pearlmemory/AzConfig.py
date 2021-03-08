7d8d8bb13b17bb45be5ce1340cff4af5242d0b23
Rebecca <rebecca@abrahim.org>
2021-03-09 07:36:49 +1100

    Adds AzConfig template

--- pearlmemory/AzConfig.py
+++ pearlmemory/AzConfig.py
@@ -1,11 +1,32 @@
+"""
+pearlmemory is a variation of french-genanki-jupyter made for German
+learners.
+
+Copyright (C) 2020  errbufferoverfl.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import os
 
 import uuid
+from pathlib import Path
 
 
 class AzConf:
     AZURE_SPEECH_KEY = os.environ.get("AZ-SPEECH-KEY")
     AZURE_TRANSLATE_KEY = os.environ.get("AZ-TRANS-KEY")
+    AZURE_SSML_CONF = Path("ssml.xml")
     BING_SEARCH_KEY = os.environ.get("AZ-SEARCH-KEY")
 
     VOICE_SUBSCRIPTION_REGION = (
